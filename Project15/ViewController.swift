2695aa76bb8dde3b428c958d2d08ba679ab54456
halloween-jack <halloween.jack.dev@gmail.com>
2017-10-20 18:00:23 +0900

    Preparing for action

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var tap: UIButton!
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Actions
+    @IBAction func tapped(_ sender: UIButton) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
 }
 
